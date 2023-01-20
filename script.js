6e5ed51bd808ce34caac06b1dcd821dc62929774
Berserk <alexlecerf0131@gmail.com>
2023-01-20 19:58:54 +0000

    added jQuery code to implement a forEach loop which cycles through hours array and added class names to new div dynamically created and append to container

--- script.js
+++ script.js
@@ -3,3 +3,35 @@ $("#currentDay").text(currentDate.format("dddd, MMMM, Do"));
 
 var container = $(".container")
 
+
+var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+
+// for (var i = 0; i < hours.length; i++) {
+    
+// var timeBlock = $("div")
+
+// timeBlock.text(hours[i])
+
+// timeBlock.attr("class", "time-block")
+
+// container.append(timeBlock)
+
+// }
+
+hours.forEach(function(time, index){
+    
+    var timeBlock = $("<div>").attr("id", index).addClass("time-block")
+// timeBlock.addClass("col-12")
+// timeBlock.addClass(index)
+timeBlock.addClass("row")
+
+timeBlock.text(time)
+
+var button = $("<button>").addClass("saveBtn")
+// button.addClass("col-6")
+
+timeBlock.append(button)
+// timeBlock.attr("class", "time-block")
+
+container.append(timeBlock)
+})
