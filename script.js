aae0e3209eefb104e7911182b4287ff26a481c03
Berserk <alexlecerf0131@gmail.com>
2023-01-22 22:03:48 +0000

    added function to dynamically create html using jQuery and added classes for each element which corresponds to unique styling in css file. Commented sections and added viable HTML to use if developer does not wish to us jQuery to create elements.

--- script.js
+++ script.js
@@ -6,8 +6,37 @@ var container = $(".container")
 
 var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
 
+
+hours.forEach(function (time, index) {
+
+    var timeBlock = $("<div>").addClass("time-block");
+    // variables declared for each part of dynamically generated html
+    var currentTime = $("<div>").addClass("hour");
+    var textArea = $("<textarea>").attr("id", index).addClass("description");
+    var button = $("<button>").addClass("saveBtn");
+
+    // adding classes for styling to be implemented
+    timeBlock.addClass("row");
+    currentTime.addClass("col-1")
+    textArea.addClass("col-10")
+    button.addClass("col-1")
+    button.addClass("fas fa-save");
+
+    // looping array to set work hours so they can be changed by other developers
+    currentTime.text(time);
+
+    // apending all dynamic elements in correct order
+    timeBlock.append(currentTime)
+    timeBlock.append(textArea);
+    timeBlock.append(button);
+    container.append(timeBlock);
+})
+
+
+// does the same as a forEach loop stated above
+
 // for (var i = 0; i < hours.length; i++) {
-    
+
 // var timeBlock = $("div")
 
 // timeBlock.text(hours[i])
@@ -17,21 +46,3 @@ var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
 // container.append(timeBlock)
 
 // }
-
-hours.forEach(function(time, index){
-    
-    var timeBlock = $("<div>").attr("id", index).addClass("time-block")
-// timeBlock.addClass("col-12")
-// timeBlock.addClass(index)
-timeBlock.addClass("row")
-
-timeBlock.text(time)
-
-var button = $("<button>").addClass("saveBtn")
-// button.addClass("col-6")
-
-timeBlock.append(button)
-// timeBlock.attr("class", "time-block")
-
-container.append(timeBlock)
-})
